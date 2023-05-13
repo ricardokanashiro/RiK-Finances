c1c4df37a6515e2a92e4ae0fc273cf016a3736e1
Ricardo <ricardo.kanashiro.adm@gmail.com>
2023-05-13 14:40:01 -0300

    Adicionando o menu mobile no header

---
+++ app.js
@@ -0,0 +1,16 @@
+const userActions = document.querySelector('.header__user-actions');
+const hamburguerButton = document.querySelector('.header__hamburger-btn');
+const closeButton = document.querySelector('.user-actions__close-menu-btn');
+
+function showUserActions() {
+    console.log('showUserActions');
+    userActions.classList.add('active-user-actions');
+}
+
+function hideUserActions() {
+    console.log('hideUserActions');
+    userActions.classList.remove('active-user-actions');
+}
+
+hamburguerButton.addEventListener('click', showUserActions);
+closeButton.addEventListener('click', hideUserActions);
