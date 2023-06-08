61a3139e7ffa0afa9b89a8f26b7004b9bbc172be
Ricardo <ricardo.kanashiro.adm@gmail.com>
2023-06-08 18:52:55 -0300

    added categories section

--- app.js
+++ app.js
@@ -1,13 +1,9 @@
 import { chartPallete } from "./utils.js";
+import { myBarChart, myPieChart } from "./utils.js";
 
 const userActions = document.querySelector('.header__user-actions');
 const hamburguerButton = document.querySelector('.header__hamburger-btn');
 const closeButton = document.querySelector('.user-actions__close-menu-btn');
-
-const reloadBtn = document.querySelector('#reloadChart');
-
-const pieChart = document.getElementById('pieChart');
-const barChart = document.getElementById('barChart');
 
 function showUserActions() {
     console.log('showUserActions');
@@ -21,59 +17,3 @@ function hideUserActions() {
 
 hamburguerButton.addEventListener('click', showUserActions);
 closeButton.addEventListener('click', hideUserActions);
-
-reloadBtn.onclick = () => {
-
-    myPieChart.data.datasets[0].data = [10, 10, 10, 10, 10, 10];
-    myPieChart.update();
-
-    myBarChart.data.datasets[0].data = [10, 10, 10, 10, 10, 10];
-    myBarChart.update();
-}
-
-const myPieChart = new Chart(pieChart, {
-    type: 'doughnut',
-    data: {
-        labels: ['Investimento', 'Reserva de Emergência', 'Periféricos', 'Roupas', 'Lazer', 'Outros'],
-        datasets: [{
-          label: 'Porcentagem por Categoria',
-          data: [12, 19, 3, 5, 2, 3],
-          borderWidth: 1,
-          backgroundColor: chartPallete,
-        }]
-    },
-
-    options: {
-      responsive: true,
-      maintainAspectRatio: false,
-    }
-});
-
-const myBarChart = new Chart(barChart, {
-    type: 'bar',
-    data: {
-        labels: ['Janeiro', 'Fevereiro', 'Março', 'Abril'],
-
-        datasets: [
-            {
-              label: "Input",
-              data: [40, 50, 60, 70],
-              backgroundColor: "#409EF7"
-            },
-
-            {
-              label: "Output",
-              data: [30, 40, 50, 60],
-              backgroundColor: "#4c49ed",
-            },
-        ],
-
-        borderWidth: 1,
-        backgroundColor: chartPallete,
-    },
-    
-    options: {
-      responsive: true,
-      maintainAspectRatio: false,
-    }
-});
--- utils.js
+++ utils.js
@@ -1,3 +1,5 @@
+// Chart Pallete
+
 const chartPallete = [
     "#409EF7",
     "#2F5CE0",
@@ -9,3 +11,57 @@ const chartPallete = [
 ];
 
 export { chartPallete };
+
+// Charts Configs
+
+const pieChart = document.getElementById('pieChart');
+const barChart = document.getElementById('barChart');
+
+const myPieChart = new Chart(pieChart, {
+    type: 'doughnut',
+    data: {
+        labels: ['Investimento', 'Reserva de Emergência', 'Periféricos', 'Roupas', 'Lazer', 'Outros'],
+        datasets: [{
+          label: 'Porcentagem por Categoria',
+          data: [12, 19, 3, 5, 2, 3],
+          borderWidth: 1,
+          backgroundColor: chartPallete,
+        }]
+    },
+
+    options: {
+      responsive: true,
+      maintainAspectRatio: false,
+    }
+});
+
+const myBarChart = new Chart(barChart, {
+    type: 'bar',
+    data: {
+        labels: ['Janeiro', 'Fevereiro', 'Março', 'Abril'],
+
+        datasets: [
+            {
+              label: "Input",
+              data: [40, 50, 60, 70],
+              backgroundColor: "#409EF7"
+            },
+
+            {
+              label: "Output",
+              data: [30, 40, 50, 60],
+              backgroundColor: "#4c49ed",
+            },
+        ],
+
+        borderWidth: 1,
+        backgroundColor: chartPallete,
+    },
+    
+    options: {
+      responsive: true,
+      maintainAspectRatio: false,
+    }
+});
+
+export { myBarChart, myPieChart };
