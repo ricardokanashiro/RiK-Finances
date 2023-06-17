e7ff2c0a38b30f454f7e0c5bb5da67182c32858c
Ricardo <ricardo.kanashiro.adm@gmail.com>
2023-06-17 15:09:27 -0300

    categories hover effect + categories delete option

--- app.js
+++ app.js
@@ -4,16 +4,31 @@ import { myBarChart, myPieChart } from "./utils.js";
 const userActions = document.querySelector('.header__user-actions');
 const hamburguerButton = document.querySelector('.header__hamburger-btn');
 const closeButton = document.querySelector('.user-actions__close-menu-btn');
+const categories = document.querySelectorAll('.categories-wrapper__category');
 
 function showUserActions() {
-    console.log('showUserActions');
     userActions.classList.add('active-user-actions');
 }
 
 function hideUserActions() {
-    console.log('hideUserActions');
     userActions.classList.remove('active-user-actions');
+}
+
+function toggleCategoryDeleteScreen(event) {
+    
+    let category = event.target;
+
+    while(category.classList[0] != 'categories-wrapper__category') {
+        console.log(category.classList[0]);
+        category = category.parentNode;
+    }
+
+    category.querySelector('.category__screen2').classList.toggle('category__screen2-active');
+
 }
 
 hamburguerButton.addEventListener('click', showUserActions);
 closeButton.addEventListener('click', hideUserActions);
+categories.forEach(category => category.addEventListener(
+    "click", (event) => toggleCategoryDeleteScreen(event)
+));
