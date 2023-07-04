606eae6dd4b1b6f4120dc3685e9b7af6ea513c12
Ricardo <ricardo.kanashiro.adm@gmail.com>
2023-07-04 19:46:02 -0300

    actions modal added & category modal updated

--- app.js
+++ app.js
@@ -3,8 +3,19 @@ import { myBarChart, myPieChart } from "./utils.js";
 
 const userActions = document.querySelector('.header__user-actions');
 const hamburguerButton = document.querySelector('.header__hamburger-btn');
+
 const closeButton = document.querySelector('.user-actions__close-menu-btn');
+
 const categories = document.querySelectorAll('.categories-wrapper__category');
+
+const categoriesModal = document.querySelector('.modal__categories__wrapper');
+const actionsModal = document.querySelector('.modal__actions__wrapper');
+
+const buttons = document.querySelectorAll('button');
+
+buttons.forEach(btn => btn.addEventListener('click', (event) => {
+    event.preventDefault();
+}));
 
 function showUserActions() {
     userActions.classList.add('active-user-actions');
@@ -27,6 +38,24 @@ function toggleCategoryDeleteScreen(event) {
         category.querySelector('.category__screen2').classList.toggle('category__screen2-active');
     }
 
+}
+
+function showModal(modal) {
+    modal.classList.toggle('activedModal');
+}
+
+function toggleModalActionBtn(event) {
+    let previousActivedButton = actionsModal.querySelector('.activedActionBtn');
+
+    if(previousActivedButton) { 
+        previousActivedButton.classList.remove('actived');
+    }
+
+    if(event.target.classList.contains('choose-action__input')) {
+        event.target.classList.add('activedActionInputBtn');
+    } else {
+        event.target.classList.add('activedActionOutputBtn');
+    }
 }
 
 hamburguerButton.addEventListener('click', showUserActions);
